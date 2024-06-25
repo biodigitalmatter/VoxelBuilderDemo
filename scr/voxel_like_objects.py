14276d08f62a19d1c3952205a2b3f2e5cdb9ef05
railgnam <mangliar.laci@gmail.com>
2024-06-25 09:49:20 +0200

    draft

---
+++ scr/voxel_like_objects.py
@@ -0,0 +1,17 @@
+# Class objects: 
+# 
+
+
+# Classes:
+
+# Layer()
+# the layers of the environment: 
+# voxel-like objects > (numpy arrays)
+
+# Agents()
+# agents reacting to and acting in the environment
+# general objects
+
+# Functions
+# diffusion mechanisms
+
