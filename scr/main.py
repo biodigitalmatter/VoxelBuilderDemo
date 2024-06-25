7206ee53da573de1f442cfb18583fa3c1a3df3bc
railgnam <57008999+railgnam@users.noreply.github.com>
2024-06-25 19:20:57 +0200

    update - fix diffusion
    diffsion from one point testing

--- scr/main.py
+++ scr/main.py
@@ -4,13 +4,16 @@ import voxel_builder_library as builder
 import show_voxel_plt as view
 import numpy as np
 
-n = 120
+n = 15
 
-smells = builder.Layer('air', voxel_size = n)
-smells.rgb = [0,1,0.2]
-smells.random(add = -0.5, crop=True, strech=2)
-# view.show_voxel(smells.array, smells.colors)
-# for i in range(5):
-smells.diffuse(repeat=1)
-# view.show_voxel(smells.array, smells.colors)
-print(smells.__repr__)
+smells = builder.Layer('air', voxel_size = n, diffusion_strength= 1 / 20)
+smells.rgb = [1,1,1]
+# smells.random(add = -0.95, crop=True, strech=1)
+a = smells.zeros()
+a[2][4][6] = 1
+smells.array = a
+view.show_voxel(smells.array, smells.colors)
+for i in range(10):
+    smells.diffuse(repeat=1)
+    view.show_voxel(smells.array, smells.colors)
+print('done\n',smells.__repr__)
