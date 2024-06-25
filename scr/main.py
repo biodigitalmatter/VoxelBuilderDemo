ffeac5c24b3f218f3b84665824467638d587020e
railgnam <mangliar.laci@gmail.com>
2024-06-25 22:05:24 +0200

    removed reintrudoction when rolling

--- scr/main.py
+++ scr/main.py
@@ -4,23 +4,25 @@ import voxel_builder_library as builder
 import show_voxel_plt as view
 import numpy as np
 
-n = 7
+n = 9
 
-smells = builder.Layer('air', voxel_size = n, diffusion_strength= 1/36 , decay = 0.8, absolute_decay= 0.001)
-smells.rgb = [0.8, 0.1, 0.2]
+smells = builder.Layer('air', voxel_size = n, diffusion_strength= 1/36 , decay = 0.9, absolute_decay= 0)
+smells.rgb = [1,0.998,0.999]
 # smells.random(add = -0.95, crop=True, strech=1)
 a = smells.zeros()
-a[2][4][6] = 1
+# a[4][4][4] = 1
+a[0][0][0] = 1
 
 smells.array = a
 view.show_voxel(smells.array, smells.colors)
-for i in range(8):
-    smells.decay_propotional()
-    smells.decay_absolute()
-    smells.diffuse()
+for i in range(5):
+    smells.decay_proportional()
+    # smells.decay_absolute()
+    smells.diffuse2(repeat=2)
     
     
     view.show_voxel(smells.array, smells.colors)
-print(np.amin(smells.array), np.amax(smells.array) )
-print(smells.array)
-print('done\n',smells.__repr__)
+# print(np.amin(smells.array), np.amax(smells.array) )
+# print(smells.array)
+print('done')
+# print('done\n',smells.__repr__)
