c94101a061a75f1ba781fceee0c73975c6a6f16f
railgnam <mangliar.laci@gmail.com>
2024-06-25 18:54:23 +0200

    update - diffuse

--- scr/main.py
+++ scr/main.py
@@ -4,17 +4,13 @@ import voxel_builder_library as builder
 import show_voxel_plt as view
 import numpy as np
 
-n = 5
+n = 120
 
 smells = builder.Layer('air', voxel_size = n)
 smells.rgb = [0,1,0.2]
-smells.random(add = -0.9, crop=True, strech=10)
-
-# print('array')
-# print(smells.array)
-# colors = smells.colors
-# print('colors')
-# print(colors)
-# print('bounds', np.max(colors), np.min(colors))
-
-view.show_voxel(smells.array, smells.colors)
+smells.random(add = -0.5, crop=True, strech=2)
+# view.show_voxel(smells.array, smells.colors)
+# for i in range(5):
+smells.diffuse(repeat=1)
+# view.show_voxel(smells.array, smells.colors)
+print(smells.__repr__)
