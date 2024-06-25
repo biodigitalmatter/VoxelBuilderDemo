768cae1c139be440c9937c17a8edfdb63768b73c
railgnam <mangliar.laci@gmail.com>
2024-06-25 13:31:42 +0200

    builder library draft
    chatgpt draft

--- scr/voxel_like_objects.py
+++ scr/voxel_like_objects.py
@@ -1,17 +1,1 @@
-# Class objects: 
-# 
-
-
-# Classes:
-
-# Layer()
-# the layers of the environment: 
-# voxel-like objects > (numpy arrays)
-
-# Agents()
-# agents reacting to and acting in the environment
-# general objects
-
-# Functions
-# diffusion mechanisms
 
