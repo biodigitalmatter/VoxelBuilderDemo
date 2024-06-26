abd21d4dca019cbc773c27896fd2f109025e686a
railgnam <mangliar.laci@gmail.com>
2024-06-26 12:09:58 +0200

    logical update

--- scr/main.py
+++ scr/main.py
@@ -12,15 +12,15 @@ smells.rgb = [1,0.998,0.999]
 # smells.random(add = -0.95, crop=True, strech=1)
 a = smells.zeros()
 # a[4][4][4] = 1
-for _ in range(5):
+for _ in range(1):
     i, j, k = np.random.randint(0, n -1, size = 3)
     a[i][j][k] = np.random.random(1) + 0.5
 smells.array = a
 # view.show_voxel(smells.array, smells.colors)
-
-for i in range(55):
-    smells.decay_proportional(randomize = True)
-    smells.diffuse2(repeat=1, randomize=True, factor = 0)
+iter = 20
+for i in range(iter):
+    smells.decay_proportional(randomize = False)
+    smells.diffuse2(repeat=1, randomize=False, factor = 0)
     i, j, k = np.random.randint(0, n -1, size = 3)
     a[i][j][k] = np.random.random(1) + 0.5
 
@@ -30,7 +30,7 @@ grading = 500
 a = np.int64(a * grading) / grading
 smells.array = a
 name = 'grade-500'
-view.show_voxel(smells.array, smells.colors, save=True, No = name)
+save_ = True
+view.show_voxel(smells.array, smells.colors, save=save_, title='voxels_test_smells', suffix = 'n-%s_iter-%s' %(smells._n, iter), bottom_line = smells.__repr__)
 
 print('done')
-# print('done\n',smells.__repr__)
