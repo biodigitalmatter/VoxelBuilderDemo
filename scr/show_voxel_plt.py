760834bee1e04eebb7fd7a7f06678edfb03bf411
railgnam <mangliar.laci@gmail.com>
2024-06-26 12:09:58 +0200

    logical update

--- scr/show_voxel_plt.py
+++ scr/show_voxel_plt.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
-
-def show_voxel(voxel_fill_3D, colors_4D = None, show = True, save = False, title = 'img', No = 1):
+from datetime import datetime
+global timestamp_now
+timestamp = datetime.now()
+timestamp_now = timestamp.strftime("%y%m%d_%H%M%S")
+def show_voxel(voxel_fill_3D, colors_4D = None, show = True, save = False, title = 'img', suffix = '', bottom_line = ''):
     # ################## visualise
     # # and plot everything
     fig = plt.figure(figsize = [4, 4], dpi = 200)
@@ -10,10 +13,10 @@ def show_voxel(voxel_fill_3D, colors_4D = None, show = True, save = False, title
     # voxels
     ax.voxels(voxel_fill_3D, facecolors = colors_4D)
 
-    # style
-    filename = __file__
+    # style    
+    fig.suptitle('%s_%s_%s.png' %(title, timestamp_now, suffix), fontsize = 5)
+    fig.text(0,2, s = str(bottom_line), fontsize = 2.5)
     
-    fig.suptitle("%s-%ds" %(title, No ), fontsize = 5)
     ax.set_xticks([])
     ax.set_yticks([])
     ax.set_zticks([])
@@ -23,7 +26,7 @@ def show_voxel(voxel_fill_3D, colors_4D = None, show = True, save = False, title
     # title = 'img'
     # No = 1
     if save:
-        plt.savefig('img/%s-%s.png' %(title, No), bbox_inches='tight', dpi = 200)
+        plt.savefig('img/%s_%s_%s.png' %(title, timestamp_now, suffix), bbox_inches='tight', dpi = 200)
         print('saved')
 
     if show:
