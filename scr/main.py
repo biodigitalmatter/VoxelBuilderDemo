828a76b209dadad0d2ab01d28e05658d39c16052
railgnam <mangliar.laci@gmail.com>
2024-06-26 02:05:00 +0200

    grading

--- scr/main.py
+++ scr/main.py
@@ -4,21 +4,33 @@ import voxel_builder_library as builder
 import show_voxel_plt as view
 import numpy as np
 
-n = 9
+n = 60
 
-smells = builder.Layer('air', voxel_size = n, diffusion_strength= 1/7 , decay = 0.0001, voxel_visibility=[0.2, 1])
+smells = builder.Layer('air', voxel_size = n, diffusion_strength= 1/7 , decay = 0.01)
+smells.rgb = [1,1,0]
 smells.rgb = [1,0.998,0.999]
 # smells.random(add = -0.95, crop=True, strech=1)
 a = smells.zeros()
 # a[4][4][4] = 1
-a[0][0][0] = 1
-
+for _ in range(5):
+    i, j, k = np.random.randint(0, n -1, size = 3)
+    a[i][j][k] = np.random.random(1) + 0.5
 smells.array = a
-view.show_voxel(smells.array, smells.colors)
-for i in range(5):
-    smells.decay_proportional(randomize = False)
-    smells.diffuse2(repeat=1, randomize=False, factor = 0)
-    view.show_voxel(smells.array, smells.colors)
+# view.show_voxel(smells.array, smells.colors)
+
+for i in range(55):
+    smells.decay_proportional(randomize = True)
+    smells.diffuse2(repeat=1, randomize=True, factor = 0)
+    i, j, k = np.random.randint(0, n -1, size = 3)
+    a[i][j][k] = np.random.random(1) + 0.5
+
+
+a = smells.array
+grading = 500
+a = np.int64(a * grading) / grading
+smells.array = a
+name = 'grade-500'
+view.show_voxel(smells.array, smells.colors, save=True, No = name)
 
 print('done')
 # print('done\n',smells.__repr__)
