04aced2755cb5e573a64bc7dcc87aeaa170d0928
railgnam <mangliar.laci@gmail.com>
2024-06-27 12:43:57 +0200

    fixed counter

--- scr/main.py
+++ scr/main.py
@@ -4,15 +4,19 @@ import voxel_builder_library as designer
 import show_voxel_plt as view
 import numpy as np
 
+# SETTINGS #
 n = 30
 iter = 5
-show_result = True
-show_animation = True
-show_nth = 1
-save_img = False
-save_anim = False
 note = 'test_animation'
 
+show_result = True
+save_img = False
+
+show_animation = True
+show_nth = 1
+save_anim = False
+
+# CREATE OBJECTS #
 smells = designer.Layer()
 smells.name = 'smells'
 smells.voxel_size = n
@@ -23,17 +27,16 @@ smells.diffusion_random_factor = 0
 smells.gradient_resolution = 0
 smells.rgb = [1,1,1]
 
-# SETUP #
+# SIMULATION SETUP #
 smells.empty_array()
-# add one drop
 
+# add one drop
 smells.array[n-1][int(n/3)][0] = 1
 # order = ('x','y','z') #!!!???
 # add random drops
 # for _ in range(2):
 #     i, j, k = np.random.randint(0, n -1, size = 3)
 #     smells.array[i][j][k] = 1 - np.random.random(1) * 0.2
-
 
 # RUN #
 def run(loop_count):
@@ -58,7 +61,6 @@ if show_result and not show_animation:
     print('plot_done')
 
 # ANIMATION
-
 if show_result and show_animation:
     from matplotlib import animation
     from matplotlib import pyplot as plt
@@ -68,19 +70,17 @@ if show_result and show_animation:
         suffix = 'n-%s_iter-%s_%s' %(smells._n, iter, note),
         bottom_line = smells.__str__()
     )
-    print(fig)
-    print(ax)
 
     # animation function
-    # global counter
-    # counter = 0
+
     def animate(frame):
         run(1)
-        # if counter % show_nth == 0:
-        view.show(fig, ax, smells, save_img=False, show_img=False)
-        # counter += 1
+        if animate.counter % show_nth == 0:
+            view.show(fig, ax, smells, save_img=False, show_img=False)
+        animate.counter += 1
 
     # run animation
+    animate.counter = 0
     anim = animation.FuncAnimation(fig, animate, frames = 50, interval = 50)
     if save_anim:
         anim.save("img/gif/test.gif")
