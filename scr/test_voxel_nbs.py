ead2676f35fb725caee3ae2935197d12314aae9a
railgnam <mangliar.laci@gmail.com>
2024-06-27 13:54:11 +0200

    move_1 direction dictonary

--- scr/test_voxel_nbs.py
+++ scr/test_voxel_nbs.py
@@ -1,26 +1,44 @@
 import numpy as np
 import show_voxel_plt as show
+from voxel_builder_library import direction_dict_np
 
-a = np.zeros(8)
-a = a.reshape(2,2,2)
-a[0,0,0] = 0.51
-a[1,0,0] = 0.02
-up = np.roll(a, 1, 0)
-down = np.roll(a, -1, 0)
-front = np.roll(a, -1, 1)
-back = np.roll(a, 1, 1)
-left = np.roll(a, -1, 2)
-right = np.roll(a, 1, 2)
+def value_at_index(array, index = [0,0,0], value = 1):
+    i,j,k = index
+    a[i][j][k] = value
+    return a
 
-nb_mask = {
-    'u' : [1, 0],
-    'd' : [-1, 0],
-    'f' : [-1, 1],
-    'b' : [1, -1],
-    'l' : [-1, 2],
-    'r' : [1, 2]}
+def get_color_array(array, n):
+    a = np.copy(array)
+    b = a.reshape([n,n,n,1])
+    b = 1 - b
+    colors = np.concatenate([b,b,b], axis = -1)
+    return colors
 
+# array
+n = 3
+a = np.zeros(n ** 3)
+a = a.reshape(n,n,n)
 
-print('initial\n', a, '\n0. Z. up\n', up, '\n1., Y, front\n', front, '\n2. X, left\n', left)
+# init pose
+pose = [1,1,1]
+a = value_at_index(a, pose, 1)
 
-show.show_voxel(a)
+# move
+direction =     'up'
+direction =     'down' 
+direction =     'left' 
+direction =     'right' 
+direction =     'front' 
+direction =     'back'
+d_vector = direction_dict_np[direction]
+
+pose_vector = np.asarray(pose)
+new_pose = pose_vector + d_vector
+print(new_pose)
+
+a = value_at_index(a, new_pose, 0.5)
+# show
+fig, ax = show.init_fig()
+colors = get_color_array(a, n)
+
+show.show_voxel(fig, ax, a, colors)
