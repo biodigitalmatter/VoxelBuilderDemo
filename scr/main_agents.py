64fd1c5822dff0308aa77f1ce67ce21f6add2f8f
railgnam <mangliar.laci@gmail.com>
2024-07-01 15:09:38 +0200

    construct works

--- scr/main_agents.py
+++ scr/main_agents.py
@@ -3,25 +3,29 @@ from show_voxel_plt import *
 
 
 voxel_size = 40
-agent_count = 4
-iterations = 40
+agent_count = 40
+iterations = 100
 save_ = True
 title_ = 'img'
 note = 'queen_ph'
 
+construct_limit_1 = 0.001
+construct_limit_2 = 0.01
+wait = 100
+
 agent_space = Layer(voxel_size=voxel_size, rgb=[0.5,0.5,0.5])
 queen_space = Layer(voxel_size=voxel_size, rgb=[0.5,0.5,0.5])
 track_layer = Layer(voxel_size=voxel_size, rgb=[.09,.08,0])
-smell_layer = Layer(voxel_size=voxel_size, rgb=[0.1,0.1,0.1], diffusion_ratio=1/7, decay_ratio=0.2)
+smell_layer = Layer(voxel_size=voxel_size, rgb=[0.1,0.1,0.1], diffusion_ratio=1/6, decay_ratio=0.05)
 offset_ph = Layer(name = 'offset_ph', voxel_size=voxel_size, rgb = [0.25, 0.25, 0.25])
 
 # create ground:
 ground_level_Z = 0
 ground = Layer(voxel_size=voxel_size, name='Ground')
 ground.array = make_solid_box_z(voxel_size, ground_level_Z)
-wall = make_solid_box_xxyyzz(voxel_size, 12,12,0,20,0,6)
-ground.array += wall
-ground.rgb = [0.025,0.025,0.025]
+# wall = make_solid_box_xxyyzz(voxel_size, 12,12,0,20,0,6)
+# ground.array += wall
+ground.rgb = [0.6,0.6,0.6]
 
 # grounds_offset
 offset_ph.decay_linear_value = 1/6
@@ -33,7 +37,8 @@ grounds_emission_value = 2
 agents = []
 for i in range(agent_count):
     agent = Agent(
-        space_layer = agent_space, ground_layer = ground, limited_to_ground = 'offset', track_layer = track_layer, leave_trace=True)
+        space_layer = agent_space, ground_layer = ground, construction_layer = ground,
+        limited_to_ground = 'neighbor', track_layer = track_layer, leave_trace=True)
     x = np.random.randint(0, voxel_size)
     y = np.random.randint(0, voxel_size)
     agent.pose = [x,y,1]
@@ -47,27 +52,33 @@ queen.update_space()
 # queen_space.array[10,10,1] = 1
 
 # prep smells
-for i in range(15):
-    smell_layer.emission_intake(queen_space.array, 1, False)
+for i in range(wait):
+    smell_layer.emission_intake(queen_space.array, 2, False)
     smell_layer.diffuse()
     smell_layer.decay()
     ground.block_layers([smell_layer])
 
 # run simulation
 for i in range(iterations):
+    # offset layer
     offset_ph.emission_intake(ground.array, grounds_emission_value, False)
     offset_ph.diffuse(False)
     offset_ph.decay_linear()
-    smell_layer.emission_intake(queen_space.array, 1, False)
+
+    # queen odor
+    smell_layer.emission_intake(queen_space.array, 2, False)
     smell_layer.diffuse()
     smell_layer.decay()
     ground.block_layers([smell_layer])
+
     #follow pheromones
     for agent in agents:
         pheromones = agent.get_nb_cell_values(smell_layer, agent.pose)
-        # print(pheromones)
-        agent.follow_pheromones(pheromones, offset_ph)
-    # path_pheromone
+        random_ph = agent.random_pheromones() * 0.05
+        agent.follow_pheromones(pheromones + random_ph, offset_limit = offset_ph)
+        built = agent.construct(smell_layer, construct_limit_1, construct_limit_2)
+        if built: print('built')
+    # make path_pheromone
     # smell_layer.emission_intake(agent_space.array, 1, False)
     
 
@@ -78,8 +89,10 @@ c2 = agent_space.color_array
 c3 = queen_space.color_array
 c4 = track_layer.color_array
 colors = (c1 + c2 + c3 + c4) /4
+ground.array[:,:,0] = 0
 
+show_layers = ground.array #+ track_layer.array
 # show image
 f,a = init_fig(suffix=note)  #bottom_line=Layer.__str__())
-show_voxel(f,a, queen_space.array + ground.array + agent_space.array + track_layer.array , save=True, suffix=note)
+show_voxel(f,a, ground.array, save=True, suffix=note)
 # show_voxel(f,a, smell_layer.array, c4, save=True, suffix=note)
