3402c89099ac8dbf821f63cb51c10f9c5e0c56be
railgnam <mangliar.laci@gmail.com>
2024-07-03 16:34:49 +0200

    move to cube_nbs

--- scr/main_agents_around_queen.py
+++ scr/main_agents_around_queen.py
@@ -5,11 +5,11 @@ from show_voxel_plt import timestamp_now
 
 
 voxel_size = 30
-agent_count = 250
-iterations = 1000
+agent_count = 20
+iterations = 800
 save_ = True
 title_ = 'img'
-note = 'a25-i1000'
+note = 'test_26_nbs'
 
 walk_method_option = ['nb_check', 'offset_ph', 'cube_corner_nb_check', 'cube_edge_nb_check'][2]
 
@@ -30,8 +30,8 @@ smell_layer = Layer(voxel_size=voxel_size, rgb=[240/255, 220/255, 150/255], diff
 ground_level_Z = 0
 ground = Layer(voxel_size=voxel_size, name='Ground')
 ground.array = make_solid_box_z(voxel_size, ground_level_Z)
-wall = make_solid_box_xxyyzz(voxel_size, 3,5,2,8,0,12)
-ground.array += wall
+# wall = make_solid_box_xxyyzz(voxel_size, 3,5,2,8,0,12)
+# ground.array += wall
 ground.rgb = [207/255, 179/255, 171/255]
 
 # grounds_offset
@@ -46,7 +46,7 @@ agents = []
 for i in range(agent_count):
     agent = Agent(
         space_layer = agent_space, ground_layer = ground, construction_layer = ground,
-        limited_to_ground = walk_method_option, track_layer = None, leave_trace=False, save_move_history=False)
+        track_layer = None, leave_trace=False, save_move_history=False)
     x = np.random.randint(0, voxel_size)
     y = np.random.randint(0, voxel_size)
     agent.pose = [x,y,1]
@@ -57,8 +57,6 @@ queens = []
 poses = [[20,20], [12,9]]
 for i in range(2):
     queen = Agent(space_layer = queen_space, ground_layer = ground)
-    # x = np.random.randint(5, voxel_size - 5)
-    # y = np.random.randint(5, voxel_size - 5)
     x,y = poses[i]
     queen.pose = [x,y,1]
     queen.update_space()
@@ -82,14 +80,16 @@ for i in range(iterations):
 
     #follow pheromones
     for agent in agents:
-        queen_pheromones = agent.get_nb_cell_values(smell_layer, agent.pose) * 2
-        random_ph = agent.random_pheromones() * 0.1
-        up_pref = agent.direction_preference_pheromones(0.8) * 0.5
-        moved = agent.follow_pheromones(random_ph + queen_pheromones + up_pref, offset_ph, check_collision)
+        # Agent.get_nb_26_cell_values
+        queen_ph = agent.get_nb_26_cell_values(smell_layer, agent.pose)
+        random_ph = agent.random_pheromones(26)
+        up_pref_ph = agent.direction_preference_26_pheromones(0.8)
+        pheromone_cube = random_ph * 2 + queen_ph * 0.1 + up_pref_ph * 0.5
+        moved = agent.follow_pheromones(pheromone_cube)
         if moved:
             # build
             if construction_on:
-                flag = agent.get_build_flag_after_pheromones(smell_layer, construct_limit_1, construct_limit_2 )
+                flag = agent.get_build_flag_by_pheromones(smell_layer, construct_limit_1, construct_limit_2 )
                 if flag:
                     built = agent.build()
                 else: built = False
@@ -120,7 +120,7 @@ a2 = agent_space.array
 a3 = queen_space.array
 # a4 = track_layer.array
 a5 = smell_layer.array
-a1[:,:,0] = 0
+# a1[:,:,0] = 0
 ground.array = a1
 
 time__ = timestamp_now
@@ -131,11 +131,12 @@ time__ = timestamp_now
 # print('ptcloud saved as %s:' %filename)
 
 # # save as point_list
-filename = 'scr/data/point_lists/pts_%s_%s.json' %(time__, note)
-with open(filename, 'w') as file:
-    list_to_dump = convert_array_to_points(ground.array, True)
-    json.dump(list_to_dump, file)
-print('\nptlist saved as %s:\n' %filename)
+if save_:
+    filename = 'scr/data/point_lists/pts_%s_%s.json' %(time__, note)
+    with open(filename, 'w') as file:
+        list_to_dump = convert_array_to_points(ground.array, True)
+        json.dump(list_to_dump, file)
+    print('\nptlist saved as %s:\n' %filename)
 
 
 # show VOXEL image
@@ -147,10 +148,10 @@ print('\nptlist saved as %s:\n' %filename)
 
 # show SCATTER PLOT
 pts_ground = convert_array_to_points(ground.array, False)
-pts_agents = convert_array_to_points(agent_space.array, False)
+pts_agent_space = convert_array_to_points(agent_space.array, False)
 
-arrays_to_show = [pts_ground]
-colors = [ground.rgb]
+arrays_to_show = [pts_ground, pts_agent_space]
+colors = [ground.rgb, agent_space.rgb]
 
 scale = voxel_size
 axes = plt.axes(xlim=(0, scale), ylim =  (0, scale), zlim = (0, scale), projection = '3d')
