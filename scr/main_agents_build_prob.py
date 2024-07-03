4ab43fc2b74c53b00c18489fa39df92f8a0697bb
railgnam <mangliar.laci@gmail.com>
2024-07-03 18:24:57 +0200

    build probability tests

--- scr/main_agents_build_prob.py
+++ scr/main_agents_build_prob.py
@@ -5,18 +5,18 @@ from show_voxel_plt import timestamp_now
 
 
 voxel_size = 30
-agent_count = 20
+agent_count = 100
 iterations = 200
 save_ = True
 title_ = 'img'
-note = 'build_prop_a%s_i%s' %(agent_count, iterations)
+note = 'build_probability_a%s_i%s' %(agent_count, iterations)
 
 construction_on = True
 # construct_limit_1 = 0.01
 # construct_limit_2 = 0.09
 construct_limit_1 = 0.005
 construct_limit_2 = 0.05
-wait = 50
+wait = 5
 
 agent_space = Layer(voxel_size=voxel_size, rgb=[34/255, 116/255, 240/255])
 queen_space = Layer(voxel_size=voxel_size, rgb=[203/255, 21/255, 207/255])
@@ -27,23 +27,30 @@ smell_layer = Layer(voxel_size=voxel_size, rgb=[240/255, 220/255, 150/255], diff
 ground_level_Z = 0
 ground = Layer(voxel_size=voxel_size, name='Ground')
 ground.array = make_solid_box_z(voxel_size, ground_level_Z)
-# wall = make_solid_box_xxyyzz(voxel_size, 3,5,2,8,0,12)
-# ground.array += wall
+wall = make_solid_box_xxyyzz(voxel_size, 3,5,2,8,0,12)
+ground.array += wall
 ground.rgb = [207/255, 179/255, 171/255]
 
-# grounds_offset
-offset_ph = Layer(name = 'offset_ph', voxel_size=voxel_size)
-offset_ph.decay_linear_value = 1/6
-offset_ph.decay_ratio = 0
-offset_ph.diffusion_ratio = 1/3
-grounds_emission_value = 2
+# move_preference settings w pheromon weights
+ground_smell_ph_w = 1
+random_ph_w = 0.2
+up_pref_ph_w = 1
+side_move_pref_ratio = 0.5
+
+# build probability settings
+add_prob = 0.2
+climb = 2
+top = 1.2
+walk = 0
+descend = -0.05
+build_probability_limit = 0.5
 
 # make agents
 agents = []
 for i in range(agent_count):
     agent = Agent(
         space_layer = agent_space, ground_layer = ground, construction_layer = ground,
-        track_layer = None, leave_trace=False, save_move_history=False)
+        track_layer = None, leave_trace=False, save_move_history=True)
     x = np.random.randint(0, voxel_size)
     y = np.random.randint(0, voxel_size)
     agent.pose = [x,y,1]
@@ -60,9 +67,9 @@ for i in range(2):
 
 # pre_smells
 for i in range(wait):
-    smell_layer.emission_intake(queen_space.array, 2, False)
+    smell_layer.emission_intake(ground.array, 2, False)
     smell_layer.diffuse()
-    smell_layer.gravity_shift(5, 1)
+    smell_layer.gravity_shift(1, 0.1)
     smell_layer.decay()
     ground.block_layers([smell_layer])
 
@@ -77,57 +84,52 @@ for i in range(iterations):
     smell_layer.decay()
     ground.block_layers([smell_layer])
 
-    #follow pheromones
+    # move and build
     for agent in agents:
-        # Agent.get_nb_26_cell_values
-        queen_ph = agent.get_nb_26_cell_values(smell_layer, agent.pose)
-        random_ph = agent.random_pheromones(26)
-        up_pref_ph = agent.direction_preference_26_pheromones(0.8)
-        pheromone_cube = random_ph * 2 + queen_ph * 0.1 + up_pref_ph * 0.5
+        reset_agent = False
+        ground_smell_ph = agent.get_nb_26_cell_values(smell_layer, agent.pose) * ground_smell_ph_w
+        random_ph = agent.random_pheromones(26) * random_ph_w
+        up_pref_ph = agent.direction_preference_26_pheromones(side_move_pref_ratio) * up_pref_ph_w
+        pheromone_cube = random_ph * 2 + ground_smell_ph + up_pref_ph * 0.5
         moved = agent.follow_pheromones(pheromone_cube)
+
+        # move based on climb style
         if moved:
+            # check move style
+            agent.analyze_move_history()
+            # add probability per style
+            agent.add_build_probability(add_prob, climb, top, walk, descend)
+
             # build
             if construction_on:
-                flag = agent.get_build_flag_by_pheromones(smell_layer, construct_limit_1, construct_limit_2 )
+                # flag = agent.get_build_flag_by_pheromones(smell_layer, construct_limit_1, construct_limit_2 )
+                flag = agent.get_build_flag_by_probability(build_probability_limit)
+                print(flag)
                 if flag:
                     built = agent.build()
-                else: built = False
-                if built: # reset position
-                    x = np.random.randint(0, voxel_size)
-                    y = np.random.randint(0, voxel_size)
-                    # print(x,y)
-                    agent.pose = [x,y,1]
-        else:
-            # couldnt move (stuck) position reset
+                    if built: reset_agent = True
+
+        elif not moved:
+            # stuck in position: reset
+            reset_agent = True
+        
+        if reset_agent:
+            x = np.random.randint(0, voxel_size)
+            y = np.random.randint(0, voxel_size)
             agent.pose = [x,y,1]
+            agent.build_probability = 0
+            agent.move_history = []
 
 print('done')
-# show smell layer in limits
-# a5 = smell_layer.array
-# smell_layer.array = np.where(construct_limit_1 <= a5, a5, 0)
-# smell_layer.array = np.where(construct_limit_2 >= smell_layer.array, smell_layer.array, 0)
 
+for agent in agents:
+    print(agent.move_history[-3:])
 
-# add layers and layer_colors
-c1 = ground.color_array
-c2 = agent_space.color_array
-c3 = queen_space.color_array
-c5 = smell_layer.color_array_inverse
-
+# SHOW
 a1 = ground.array
-a2 = agent_space.array
-a3 = queen_space.array
-# a4 = track_layer.array
-a5 = smell_layer.array
 a1[:,:,0] = 0
 ground.array = a1
-
 time__ = timestamp_now
-# # save as pointcloud
-# filename = 'scr/data/compas_pointclouds/ptcloud_%s_%s.json' %(time__, note)
-# ptcloud = convert_array_to_compas_pointcloud(a1)
-# save_ptcloud(ptcloud, filename)
-# print('ptcloud saved as %s:' %filename)
 
 # # save as point_list
 if save_:
@@ -137,18 +139,9 @@ if save_:
         json.dump(list_to_dump, file)
     print('\nptlist saved as %s:\n' %filename)
 
-
-# show VOXEL image
-# f,a = init_fig(suffix=note)  #bottom_line=Layer.__str__())
-# show_voxel(f,a, a1 + a2 + a3, c1 + c2 + c3, save=save_, suffix=note)
-# show_voxel(f,a, a1 + a2 + a3 + a5, c1 + c2 + c3 + c5, save=True, suffix=note)
-# show_voxel(f,a, a1 + a5, c1 + c5, save=save_, suffix=note + '_smells')
-# show_voxel(f,a, a1 + a3, c1 + c3, save=save_, suffix=note)
-
 # show SCATTER PLOT
 pts_ground = convert_array_to_points(ground.array, False)
 pts_agent_space = convert_array_to_points(agent_space.array, False)
-
 # arrays_to_show = [pts_ground]
 # colors = [ground.rgb]
 arrays_to_show = [pts_ground, pts_agent_space]
